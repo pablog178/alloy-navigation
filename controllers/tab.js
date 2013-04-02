37d88b8241cd71feb11c6e63d8a0b12c3e98630c
Pablo Guevara <pablog.178@gmail.com>
2013-04-02 10:49:34 -0600

    Updated styles for tab component. Removed default styles

--- controllers/tab.js
+++ controllers/tab.js
@@ -1,6 +1,11 @@
 var args = arguments[0] || {};
 
-args.style && $.wrapper.applyProperties(args.style);
+if(args.style){
+	$.wrapper.applyProperties(args.style);
+	args.style.icon && $.icon.applyProperties(args.style.icon);
+	args.style.title && $.title.applyProperties(args.style.title);
+}
+
 
 $.title.text = args.title || "";
 $.icon.image = args.icon || "";
