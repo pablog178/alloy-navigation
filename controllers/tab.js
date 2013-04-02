0857f9b92189a5f75a62bd11babd5bacffef12dd
Pablo Guevara <pablog.178@gmail.com>
2013-04-02 13:35:05 -0600

    Added wrapper for tab components

--- controllers/tab.js
+++ controllers/tab.js
@@ -1,13 +1,16 @@
 var args = arguments[0] || {};
 
 if(args.style){
-	$.wrapper.applyProperties(args.style);
+	$.tab.applyProperties(args.style);
+	args.style.wrapper && $.wrapper.applyProperties(args.style.wrapper);
 	args.style.icon && $.icon.applyProperties(args.style.icon);
 	args.style.title && $.title.applyProperties(args.style.title);
+	args.style.separator && $.separator.applyProperties(args.style.separator);
+	args.style.hideSeparator && $.tab.remove($.separator);
 }
 
 
 $.title.text = args.title || "";
 $.icon.image = args.icon || "";
 $.icon.selectedImage = args.selectedIcon || "";
-$.wrapper.tabIndex = args.tabIndex;
+$.tab.tabIndex = args.tabIndex;
